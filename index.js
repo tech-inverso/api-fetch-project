aead31b3b7872bbf615c9065350ce87678c1e3c0
El_Giancar <giancarmine.cuofano@gmail.com>
2024-11-06 14:23:17 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Importing required libraries
+const cron = require("node-cron");
+const express = require("express");
+const axios = require("axios");
+
+// Define the API endpoint
+const url = "https://inverso-backend.onrender.com/api/items?populate=*&pagination[pageSize]=5";
+
+// Function to fetch data from the API
+async function fetchData() {
+  try {
+    const response = await axios.get(url);
+    console.log("Data fetched at", new Date());
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+app = express(); // Initializing app
+
+// Creating a cron job which runs on every 10 second
+cron.schedule("*/15 * * * *", function () {
+  console.log("Running a task every 15 minutes");
+  fetchData();
+});
+
+app.listen(3000);
